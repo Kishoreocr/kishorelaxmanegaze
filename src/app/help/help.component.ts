b51dc03bc9b82c1995626f96ece5dbc755d9bba5
kishore <madishetty.kishore@gmail.com>
2019-02-09 02:58:32 +0530

    testimonia added

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
-
+data:any[]=[{"id":"tab1","key":"dddddd1111","va":"yes1"},{"id":"tab11","key":"dddddd22221","va":"yes"}];
   constructor() { }
 
   ngOnInit() {
