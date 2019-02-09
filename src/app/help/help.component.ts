500993c010e743e6c822a4f4b94b15262e58d7e2
kishore <madishetty.kishore@gmail.com>
2019-02-09 21:38:05 +0530

    changes for faqs

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
-  data: any[] = [{ "id": "tab1", "key": "I cannot find a package that suits me", "va": "Please select custom package and our Admin team will work with you to create a suitable package based on your requirements" }, 
+  public data: any[] = [{ "id": "tab1", "key": "I cannot find a package that suits me", "va": "Please select custom package and our Admin team will work with you to create a suitable package based on your requirements" }, 
   { "id": "tab2", "key": "Do you offer management services?", "va": "Management services will be provided upon request" }];
+
+  showIcon:any;
   constructor() { }
 
   ngOnInit() {
   }
 
+  chaneIcon(item){
+     item.id = !item.id;
+  }
 }
