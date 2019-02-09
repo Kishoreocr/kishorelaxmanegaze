33d25aca6bf310076fc9a5348432e5c7b202c903
kishore <madishetty.kishore@gmail.com>
2019-02-09 18:31:26 +0530

    hep changes

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
-data:any[]=[{"id":"tab1","key":"dddddd1111","va":"yes1"},{"id":"tab11","key":"dddddd22221","va":"yes"}];
+  data: any[] = [{ "id": "tab1", "key": "I cannot find a package that suits me", "va": "Please select custom package and our Admin team will work with you to create a suitable package based on your requirements" }, 
+  { "id": "tab2", "key": "Do you offer management services?", "va": "Management services will be provided upon request" }];
   constructor() { }
 
   ngOnInit() {
